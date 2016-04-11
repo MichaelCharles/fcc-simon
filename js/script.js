1d9850398710acbb3ff1398aa1de5693315ccc50
Michael Aubrey <mcaubrey511@gmail.com>
2016-04-11 15:23:40 +0000

    worked on interface, added colors

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* global $ */
+
+var soundBank = {
+        red: new Audio("audio/red.mp3"),
+        yellow: new Audio("audio/yellow.mp3"),
+        green: new Audio("audio/green.mp3"),
+        blue: new Audio("audio/blue.mp3"),
+        batsu: new Audio("audio/buzzer.mp3"),
+        maru: new Audio("audio/win.wav")
+    },
+    simon = {
+        
+    }
+
+$(document).ready(function(){
+
+})
