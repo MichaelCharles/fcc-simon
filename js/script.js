3ab9266f647599e5447d10f80a4c9e20f62df04c
Michael Aubrey <mcaubrey511@gmail.com>
2016-04-18 03:19:30 +0000

    added sound functionality

--- js/script.js
+++ js/script.js
@@ -9,9 +9,91 @@ var soundBank = {
         maru: new Audio("audio/win.wav")
     },
     simon = {
-        
+        isOn: false,
+        isStrict: false,
+        count: 0,
+        pattern: [],
+        reset: function() {
+            this.isStrict = false;
+            this.count = 0;
+            this.pattern = [];
+        },
+        input: {
+            green: function() {
+                if (simon.isOn) {
+                    soundBank.green.play();
+                }
+            },
+            red: function() {
+                if (simon.isOn) {
+                    soundBank.red.play();
+                }
+            },
+            yellow: function() {
+                if (simon.isOn) {
+                    soundBank.yellow.play();
+                }
+            },
+            blue: function() {
+                if (simon.isOn) {
+                    soundBank.blue.play();
+                }
+            },
+            strict: function() {
+                if (this.isOn){this.isStrict = !this.isStrict}
+            },
+            power: function() {
+                this.isOn = !this.isOn;
+                if (this.isOn === false) {
+                    this.reset();
+                }
+            }
+        }
     }
+
+function lightUp(element) {
+    var currentBg = element.css("background-color").replace(/[^0-9$.,]/g, '').split(",");
+    var lightBg = currentBg.map(function(value) {
+        value = parseInt(value) + 50;
+        if (value > 255) value = 255;
+        return value;
+    }); 
+    var rgbString = "rgb(" + currentBg.join(", ") + ")";
+    var lightRgbString = "rgb(" + lightBg.join(", ") + ")";
+    element.css({
+        "background-color": lightRgbString
+    });
+    setTimeout(function(){
+        element.css({
+            "background-color": rgbString
+        })
+    }, 400);
+}
 
 $(document).ready(function(){
 
+$("#green-button").click(function(){
+    simon.input.green();
+    if (simon.isOn) lightUp($(this));
+});
+$("#red-button").click(function(){
+    simon.input.red();
+    if (simon.isOn) lightUp($(this));
+});
+$("#yellow-button").click(function(){
+    simon.input.yellow();
+    if (simon.isOn) lightUp($(this));
+});
+$("#blue-button").click(function(){
+    simon.input.blue();
+    if (simon.isOn) lightUp($(this));
+});
+$("#switch").click(function(){
+    simon.isOn = !simon.isOn;
+    $("#strict-mode").prop("disabled", !$("#strict-mode").prop("disabled"));
+});
+$("strict-mode").click(function(){
+    if (simon.isOn) simon.input.strict();
 })
+
+});
