91b4df338622e3c207b796dff8f46b8afd81a857
Michael Aubrey <mcaubrey511@gmail.com>
2016-04-19 04:15:05 +0000

    uhh... I think I broke it.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,106 @@
 /* global $ */
-
+// soundBank ojbect contains all sounds for this project.
 var soundBank = {
         red: new Audio("audio/red.mp3"),
         yellow: new Audio("audio/yellow.mp3"),
         green: new Audio("audio/green.mp3"),
         blue: new Audio("audio/blue.mp3"),
-        batsu: new Audio("audio/buzzer.mp3"),
-        maru: new Audio("audio/win.wav")
+        buzzer: new Audio("audio/buzzer.mp3"),
+        win: new Audio("audio/win.wav")
     },
+    // simon object should have most (if not all) calculations.
+    // I want this to be more or a less an engine for the game,
+    // that could be made to work with an alternative UI with
+    // very little effort.
     simon = {
-        isOn: false,
-        isStrict: false,
-        isInGame: false,
-        pattern: [],
-        count: 0,
-        index: 0,
-        reset: function() {
+        isOn: false, // Is the game on or off?
+        isStrict: false, // Is the game in strict mode?
+        isPlayerInput: true, // Is the input being given by the player?
+        isAcceptingInput: true, // Is simon accepting input at the moment?
+        pattern: [], // This contains the current pattern.
+        count: 0, // This should equal the length of the pattern array.
+        index: 0, // This should represent the index in the array that the player is currently guessing.
+        choices: ["red", "yellow", "green", "blue"], // These are the available choices.
+        advance: function() { // This function advances the game.
+            if (simon.count >= 1) {
+                simon.isAcceptingInput = false;
+
+                soundBank.win.onended = function() {
+                    simon.reset();
+                }
+                soundBank.win.play();
+            } else {
+            var roll = Math.floor(Math.random() * 4);
+            var choice = simon.choices[roll];
+            
+            simon.pattern.push(choice);
+            simon.playPattern();
+            simon.count += 1;
+            }
+        },
+        playPattern: function() {
+            var patternIndex = 0;
+            simon.isAcceptingInput = false;
+            simon.isPlayerInput = false;
+            var patternPlayer = setInterval(function() {
+                simon.isAcceptingInput = true;
+                $("#" + simon.pattern[patternIndex] + "-button").click();
+                simon.isAcceptingInput = false;
+                patternIndex++;
+                if (patternIndex >= simon.pattern.length) {
+                    simon.isAcceptingInput = true;
+                    simon.isPlayerInput = true;
+                    if (simon.count < 10) {
+                        $("#count-display").text("0" + simon.count);
+                    } else {
+                        $("#count-display").text(simon.count);
+                    }
+                    clearInterval(patternPlayer);
+                }
+            }, 1000);
+        },
+        reset: function() { //This function resets the game.
             simon.isStrict = false;
             simon.pattern = [];
-            simon.isInGame = false;
+            simon.isAcceptingInput = true;
             simon.count = 0;
+            simon.index = 0;
+            simon.isOn = true;
+            simon.isPlayerInput = true;
+            $("#count-display").text(simon.count);
         },
+        // this object handles the different kinds of input that simon can receive
         input: {
-            green: function() {
+            button: function(color) {
                 if (simon.isOn) {
-                    soundBank.green.play();
-                }
-            },
-            red: function() {
-                if (simon.isOn) {
-                    soundBank.red.play();
-                }
-            },
-            yellow: function() {
-                if (simon.isOn) {
-                    soundBank.yellow.play();
-                }
-            },
-            blue: function() {
-                if (simon.isOn) {
-                    soundBank.blue.play();
+
+                    if (simon.isPlayerInput && simon.pattern.length) {
+                        // check against pattern
+                        if (color !== simon.pattern[simon.index]) {
+                            soundBank.buzzer.onended = function() {
+                                simon.index = 0;
+                                simon.playPattern();
+                            }
+                            soundBank.buzzer.play();
+                        } else {
+                            soundBank[color].play();
+                            soundBank[color].onended = function() {
+                                simon.index += 1
+                                if (simon.index >= simon.pattern.length) {
+                                    simon.index = 0;
+                                    simon.advance();
+                                }
+                            }
+                        }
+                    } else {
+                        soundBank[color].play();
+                    }
                 }
             },
             strict: function() {
-                if (this.isOn){this.isStrict = !this.isStrict}
+                if (this.isOn) {
+                    this.isStrict = !this.isStrict
+                }
             },
             power: function() {
                 simon.isOn = !simon.isOn;
@@ -52,7 +109,8 @@ var soundBank = {
                 }
             },
             start: function() {
-                
+                simon.pattern = [];
+                simon.advance();
             }
         }
     }
@@ -63,77 +121,91 @@ function lightUp(element) {
         value = parseInt(value) + 50;
         if (value > 255) value = 255;
         return value;
-    }); 
+    });
     var rgbString = "rgb(" + currentBg.join(", ") + ")";
     var lightRgbString = "rgb(" + lightBg.join(", ") + ")";
     element.css({
         "background-color": lightRgbString
     });
-    setTimeout(function(){
+    simon.isAcceptingInput = false;
+    setTimeout(function() {
         element.css({
             "background-color": rgbString
-        })
+        });
+        if (simon.isPlayerInput) simon.isAcceptingInput = true;
     }, 400);
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-$("#green-button").click(function(){
-    simon.input.green();
-    if (simon.isOn) lightUp($(this));
-});
-$("#red-button").click(function(){
-    simon.input.red();
-    if (simon.isOn) lightUp($(this));
-});
-$("#yellow-button").click(function(){
-    simon.input.yellow();
-    if (simon.isOn) lightUp($(this));
-});
-$("#blue-button").click(function(){
-    simon.input.blue();
-    if (simon.isOn) lightUp($(this));
-});
-$("#switch").click(function(){
-    var $label = $("#strict-label");
-    var $sm = $("#strict-mode");
-    var $btn = $("#start-game");
-    
-    // If Strict Mode is checked, uncheck it.
-    if ($label.hasClass("is-checked")) {
-        $sm.click();
-    }
-    
-    // If Strict Mode is enabled, disable it.
-    $sm.prop("disabled", !$sm.prop("disabled"));
-    
-    if ($label.hasClass("is-disabled")) {
-        $label.removeClass("is-disabled");
-    } else {
-        $label.addClass("is-disabled");
-    }
-    
-    // If the "Start Game" button is enabled, disable it. If disabled, enable it.
-    $btn.prop("disabled", !$btn.prop("disabled"));
-    
-    if ($btn.prop("disabled")) {
-        $btn.addClass("mdl-button--disabled");
-    } else {
-        $btn.removeClass("mdl-button--disabled");
-    }
-    
-    // Fire the "Power Button" input on simon.
-    simon.input.power();
-});
-$("#strict-mode").click(function(){
-    if (simon.isOn) simon.input.strict();
-})
-$("#start-game").click(function(){
-    simon.input.start();
-})
+    $("#green-button").click(function() {
+        if (simon.isAcceptingInput && simon.isOn) {
+            simon.input.button("green");
+            lightUp($(this));
+        }
+    });
+    $("#red-button").click(function() {
+        if (simon.isAcceptingInput && simon.isOn) {
+            simon.input.button("red");
+            lightUp($(this));
+        }
+    });
+    $("#yellow-button").click(function() {
+        if (simon.isAcceptingInput && simon.isOn) {
+            simon.input.button("yellow");
+            lightUp($(this));
+        }
+    });
+    $("#blue-button").click(function() {
+        if (simon.isAcceptingInput && simon.isOn) {
+            simon.input.button("blue");
+            lightUp($(this));
+        }
+    });
+    $("#switch").click(function(event) {
+        if (simon.isAcceptingInput) {
+            var $label = $("#strict-label");
+            var $sm = $("#strict-mode");
+            var $btn = $("#start-game");
 
-$(".big-circle").click(function() {
-    $("#count-display").text(simon.count);
-})
+            // If Strict Mode is checked, uncheck it.
+            if ($label.hasClass("is-checked")) {
+                $sm.click();
+            }
+
+            // If Strict Mode is enabled, disable it.
+            $sm.prop("disabled", !$sm.prop("disabled"));
+
+            if ($label.hasClass("is-disabled")) {
+                $label.removeClass("is-disabled");
+            } else {
+                $label.addClass("is-disabled");
+            }
+
+            // If the "Start Game" button is enabled, disable it. If disabled, enable it.
+            $btn.prop("disabled", !$btn.prop("disabled"));
+
+            if ($btn.prop("disabled")) {
+                $btn.addClass("mdl-button--disabled");
+            } else {
+                $btn.removeClass("mdl-button--disabled");
+            }
+
+            // Fire the "Power Button" input on simon.
+            simon.input.power();
+        } else {
+            event.preventDefault();
+        }
+    });
+    $("#strict-mode").click(function() {
+        if (simon.isAcceptingInput) {
+            if (simon.isOn) simon.input.strict();
+        }
+    })
+    $("#start-game").click(function() {
+        if (simon.isAcceptingInput) {
+            simon.input.start();
+        }
+    })
 
 });
